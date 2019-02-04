f1a40acc783863f5bb0ebb35f251c26ba9a0bdfa
Leslie Shea <leslieyshea@gmail.com>
2019-02-04 11:32:48 -0800

    added yes and no

--- src/apply.js
+++ src/apply.js
@@ -1,14 +1,31 @@
 const formNode = document.getElementById('form-example');
 const nameNode = document.getElementById('name');
 const zipCodeNode = document.getElementById('zip-code');
+const peanutRangeNode = document.getElementById('peanuts');
+const allergicNumberNode = document.getElementById('allergic-number');
+const yesNode = document.getElementById('yes');
+const noNode = document.getElementById('no');
 
+
+peanutRangeNode.addEventListener('change', function() {
+    allergicNumberNode.textContent = peanutRangeNode.value;
+});
+
+yesNode.addEventListener('change', function() {
+    console.log('yes', yesNode.checked);
+});
+
+noNode.addEventListener('change', function() {
+    console.log('no', noNode.checked);
+});
 
 formNode.addEventListener('submit', function(event) {
     event.preventDefault();
 
     const applicant = {
         name: nameNode.value,
-        zipCode: zipCodeNode.value
+        zipCode: zipCodeNode.value,
+        allergyScale: peanutRangeNode.value
     };
 
     console.log(applicant);
