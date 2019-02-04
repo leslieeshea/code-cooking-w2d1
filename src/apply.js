cd8320db29595d38f62772b9b69064001d1c8296
Leslie Shea <leslieyshea@gmail.com>
2019-02-04 13:30:46 -0800

    updated second scale section

--- src/apply.js
+++ src/apply.js
@@ -5,6 +5,7 @@ const peanutRangeNode = document.getElementById('peanuts');
 const allergicNumberNode = document.getElementById('allergic-number');
 const yesNode = document.getElementById('yes');
 const noNode = document.getElementById('no');
+const peanutNodeTwo = document.getElementById('peanuts-2');
 
 
 peanutRangeNode.addEventListener('change', function() {
@@ -12,22 +13,22 @@ peanutRangeNode.addEventListener('change', function() {
 });
 
 yesNode.addEventListener('change', function() {
-    console.log('yes', yesNode.checked);
     if(yesNode.checked) {
         allergicNumberNode.textContent = 1; // default to 1
         peanutRangeNode.min = 1;
         peanutRangeNode.value = 1; // default to value 1
         peanutRangeNode.disabled = false;
+        peanutNodeTwo.required = yesNode.checked;
     }
 });
 
 noNode.addEventListener('change', function() {
-    console.log('no', noNode.checked);
     if(noNode.checked) {
         allergicNumberNode.textContent = 0;
         peanutRangeNode.min = 0;
         peanutRangeNode.value = 0;
         peanutRangeNode.disabled = true;
+        peanutNodeTwo.required = !noNode.checked;
     }
 });
 
@@ -37,7 +38,8 @@ formNode.addEventListener('submit', function(event) {
     const applicant = {
         name: nameNode.value,
         zipCode: zipCodeNode.value,
-        allergyScale: peanutRangeNode.value
+        allergyScale: peanutRangeNode.value,
+        allergyScaleTwo: peanutNodeTwo.value
     };
 
     console.log(applicant);
