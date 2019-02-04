75d335042be7c64f0afadeb13bb92083b3a23197
Leslie Shea <leslieyshea@gmail.com>
2019-02-04 13:19:20 -0800

    updated range

--- src/apply.js
+++ src/apply.js
@@ -14,13 +14,21 @@ peanutRangeNode.addEventListener('change', function() {
 yesNode.addEventListener('change', function() {
     console.log('yes', yesNode.checked);
     if(yesNode.checked) {
-        allergicNumberNode.textContent = 1;
-        peanutRangeNode.value = 1;
+        allergicNumberNode.textContent = 1; // default to 1
+        peanutRangeNode.min = 1;
+        peanutRangeNode.value = 1; // default to value 1
+        peanutRangeNode.disabled = false;
     }
 });
 
 noNode.addEventListener('change', function() {
     console.log('no', noNode.checked);
+    if(noNode.checked) {
+        allergicNumberNode.textContent = 0;
+        peanutRangeNode.min = 0;
+        peanutRangeNode.value = 0;
+        peanutRangeNode.disabled = true;
+    }
 });
 
 formNode.addEventListener('submit', function(event) {
